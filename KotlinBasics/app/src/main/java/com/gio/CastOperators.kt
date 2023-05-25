4acd23287f3c7281d4a3f53b8c499eacabaa6dfa
GiCharkviani <gcharkvi@broadinstitute.org>
2023-05-25 21:21:17 +0400

    kj

--- KotlinBasics/app/src/main/java/com/gio/CastOperators.kt
+++ KotlinBasics/app/src/main/java/com/gio/CastOperators.kt
@@ -1,5 +1,14 @@
 package com.gio
 
 fun main() {
+    val obj: Any? = null
+    // unsafe
+//    val str: String = obj as String
+    // safe - as? - save cast operator
+    val str: String? = obj as? String
+    val str2: String? = obj as String?
+
+    println(str)
+    println(str2)
 
 }
