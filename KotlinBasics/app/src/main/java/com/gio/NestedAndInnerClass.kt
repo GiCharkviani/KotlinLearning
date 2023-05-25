274724bd7b3d0534f465c9c5e45dbf6f55a16b8a
GiCharkviani <gcharkvi@broadinstitute.org>
2023-05-25 21:13:30 +0400

    dsads

--- KotlinBasics/app/src/main/java/com/gio/NestedAndInnerClass.kt
+++ KotlinBasics/app/src/main/java/com/gio/NestedAndInnerClass.kt
@@ -1,5 +1,32 @@
 package com.gio
 
-fun main() {
-    
+fun main(args: Array<String>) {
+    println(OuterCLass.NestedClass().description)
+
+    var obj = OuterCLass.NestedClass() // object creation
+    obj.foo()
+
+//    var obj1 = OuterCLass.InnerClass() // object creation
+//    obj1.foo
+}
+
+class OuterCLass {
+    private var name: String = "MR X"
+    class NestedClass {
+        var description: String = "Code inside"
+        private var id: Int = 101
+        fun foo() {
+//            println("name is $name") // cannot access  the outer class member
+            println("Id is $id")
+        }
+    }
+
+    inner class InnerClass {
+        var description: String = "Code inside"
+        private var id: Int = 101
+        fun foo() {
+            println("name is $name") // can access  the outer class member
+            println("Id is $id")
+        }
+    }
 }
