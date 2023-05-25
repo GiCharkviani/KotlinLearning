a533f4f036e53f05a192175b14740b3c404a1755
GiCharkviani <gcharkvi@broadinstitute.org>
2023-05-25 14:57:53 +0400

    sasa

--- KotlinBasics/app/src/main/java/com/gio/dataClass.kt
+++ KotlinBasics/app/src/main/java/com/gio/dataClass.kt
@@ -1,16 +1,16 @@
 package com.gio
 
 // should have at least one parameter
-data class User(val id: Long, var name: String)
+data class UserData(val id: Long, var name: String)
 
 fun main() {
-    val user1 = User(1, "Gis")
+    val user1 = UserData(1, "Gis")
     val user1Name = user1.name
     println(user1Name)
 //    user1.id = 2 // it won't work as the parameter is val
     user1.name = "Gio"
 
-    val user2 = User(1, "Gio")
+    val user2 = UserData(1, "Gio")
 
     println(user1.equals(user2)) // they will equal if they have the same values in fields
     println(user1 == user2) // the same
